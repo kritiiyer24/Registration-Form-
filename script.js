60d4789696af872932e532b1d66207b8c41b0d56
kritiiyer24 <kritiiyer042010@gmail.com>
2025-10-07 17:16:04 -0400

    Registration Form

---
+++ script.js
@@ -0,0 +1,34 @@
+function validate(){ 
+    var name=document.getElementById("name").value;
+    var email=document.getElementById("email").value;
+    var age=document.getElementById("age").value;
+
+if(name==="")
+{ 
+    document.getElementById("nameError").innerHTML="Name Cannot Be Empty"; 
+    document.getElementById("nameError").style="color:red";
+}
+
+else{ 
+    document.getElementById("nameError").innerHTML=""; 
+}
+if(email==="")
+{ 
+    document.getElementById("emailError").innerHTML="Email Cannot Be Empty"; 
+    document.getElementById("emailError").style="color:red";
+}
+
+else{ 
+    document.getElementById("emailError").innerHTML=""; 
+}
+
+if(age==="")
+{ 
+    document.getElementById("ageError").innerHTML="Age Cannot Be Empty"; 
+    document.getElementById("ageError").style="color:red";
+}
+
+else{ 
+    document.getElementById("ageError").innerHTML=""; 
+} 
+} 
